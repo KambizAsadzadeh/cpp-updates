81622a3340383c65502defd8ccb52aa8d925b153
Kambiz Asadzadeh <kambiz.ceo@gmail.com>
2023-04-16 00:31:42 +0330

    Added accumulate example.

---
+++ accumulate.cpp
@@ -0,0 +1,25 @@
+//!----> Pre-C++20
+#include <iostream>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int sum = 0;
+
+    for (int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+    std::cout << "Sum = " << sum << std::endl;
+    return 0;
+}
+
+//!----> C++20
+#include <iostream>
+#include <numeric>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int sum = std::accumulate(arr, arr + 5, 0);
+    std::cout << "Sum = " << sum << std::endl;
+    return 0;
+}
