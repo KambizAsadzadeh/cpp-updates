9b2294e3e1de63fd4349c330885c5f0513a6435c
Kambiz Asadzadeh <kambiz.ceo@gmail.com>
2023-04-16 16:01:46 +0330

    Added ranges 08 (keys,value and all)

---
+++ ranges-08.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ranges>
+#include <map>
+
+int main()
+{
+    std::map<std::string, std::string> data
+        {
+         {"name", "C++"},
+         {"informal_name", "C++2a"},
+         {"standard", "ISO/IEC 14882:2020"},
+         };
+
+    //!Get keys
+    for (auto const& key : std::views::keys(data))
+        std::cout << key << '\n';
+
+    //!Get values
+    for (auto const& value : std::views::values(data))
+        std::cout << value << '\n';
+}
+
+#include <iostream>
+#include <ranges>
+#include <map>
+
+int main()
+{
+    std::map<std::string, std::string> data
+        {
+         {"name", "C++"},
+         {"informal_name", "C++2a"},
+         {"standard", "ISO/IEC 14882:2020"},
+         };
+
+    for (auto const& get : std::views::all(data))
+        std::cout << get.first << "" << get.second << '\n';
+}
