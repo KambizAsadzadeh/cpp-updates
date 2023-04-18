c46655951c3f529dba0528aa4981315d064f3619
Kambiz Asadzadeh <kambiz.ceo@gmail.com>
2023-04-18 07:36:17 +0330

    Update concepts-04.cpp

--- concepts-04.cpp
+++ concepts-04.cpp
@@ -14,17 +14,22 @@ struct Person final
 /*!
  * \brief The Date class
  */
-class Date {};
+class Date {
+public:
+    std::string date {};
+};
 
 /*!
  * \brief The PaymentTerms class
  */
-struct PaymentTerms {};
+struct PaymentTerms {
+    bool warranty;
+};
 
 /*!
  * \brief The FactorStruct class
  */
-class FactorStruct final
+struct FactorStruct final
 {
     unsigned int    uniqueId            {};
 
@@ -42,28 +47,75 @@ class FactorStruct final
 
 class BaseFactor {
 public:
-    BaseFactor(const FactorStruct& factor): m_factor(factor) {}
+    BaseFactor(){}
+    BaseFactor(const FactorStruct& factor) {}
+    virtual ~BaseFactor(){}
+    virtual void print() = 0;
+};
+
+class NewFactor : public BaseFactor {
+public:
+    NewFactor(){}
+    NewFactor(const FactorStruct& factor){}
+    void print() override
+    {
+        std::cout << m_factor.title << std::endl;
+    }
 private:
     FactorStruct m_factor;
 };
 
 class MyFactor {
 public:
-    MyFactor(const BaseFactor& factor): m_factor(factor) {}
-
+    MyFactor(){}
+    MyFactor(const NewFactor& factor){}
 private:
-    BaseFactor m_factor;
+    NewFactor m_factor;
+};
+
+class MyFactorTmp {
+public:
+    MyFactorTmp(){}
+    MyFactorTmp(const NewFactor& factor){}
+private:
+    std::string fakeName() { return "I'm Fake!"; }
 };
 
 int main() {
 
-    if constexpr (std::constructible_from<MyFactor, BaseFactor>) {
-        std::cout << "MyFactor is constructible from BaseFactor.\n";
-    } else {
-        std::cout << "MyFactor is not constructible from BaseFactor.\n";
+    auto factorData = FactorStruct(); //!My factor information
+
+    Person          p   {   .name = "Kambiz", .family = "Asadzadeh" };
+    Date            d   {   .date = "2023-04-18" };
+    PaymentTerms    pt  {   .warranty = true };
+
+    //!Factor data initializing...
+    factorData.uniqueId = 1;
+    factorData.title = "Apple Service";
+    factorData.buyerName = p;
+    factorData.description = "Apple iMac 21 arm64 (M1)";
+    factorData.invoiceIssuanceDate = d;
+    factorData.totalAmount = 2'000;
+                             factorData.terms = pt;
+
+    auto myFactor = MyFactor(factorData); //!My new factor
+
+    auto newFactor = NewFactor(factorData);
+
+    auto tmpFactor = MyFactorTmp(factorData);
+
+    if constexpr (std::constructible_from<decltype(myFactor), decltype(newFactor)>)
+    {
+        std::cout << " It's Standard factor.\n";
+    }
+    else
+    {
+        std::cout << " It's not Standard factor, please choose another one!\n";
     }
 
-    static_assert(std::constructible_from<MyFactor, BaseFactor> == true);
+    static_assert(std::constructible_from<MyFactor, NewFactor> == true );
+    static_assert(std::constructible_from<decltype(myFactor), decltype(newFactor)> == true );
+    static_assert(std::constructible_from<decltype(myFactor), decltype(tmpFactor)> == true );
 
     return 0;
 }
